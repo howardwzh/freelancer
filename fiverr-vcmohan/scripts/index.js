c2f681d734097710fb97cd2f58cc73cb0cde0c2e
howardwzh <howardwzh@163.com>
2019-09-25 00:50:34 +0800

    feature: develop parallax scroll page for vcmohan

---
+++ fiverr-vcmohan/scripts/index.js
@@ -0,0 +1,21 @@
+var sectionScenes = [];
+
+function loaded () {
+  // cache scenes
+  sectionScenes.push(document.getElementById('scene1'))
+  sectionScenes.push(document.getElementById('scene2'))
+  // init iscroll
+  myScroll = new IScroll('#wrapper', { probeType: 3, mouseWheel: true, snap: true });
+  myScroll.on('scroll', handleScroll);
+  sectionScenes[0].className = 'scene scene-in'
+}
+
+function handleScroll () {
+  const index = this.currentPage.pageY
+  sectionScenes[index].className = 'scene scene-in'
+  for (var i = 0; i < sectionScenes.length; i++) {
+    if (i !== index) {
+      sectionScenes[i].className = 'scene'
+    }
+  }
+}
