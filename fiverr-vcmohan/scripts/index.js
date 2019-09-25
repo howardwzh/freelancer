12f517a539e24ef932341027d067da09c68a2e33
howardwzh <howardwzh@163.com>
2019-09-25 14:37:31 +0800

    feature: develop parallax scroll page for vcmohan 05

--- fiverr-vcmohan/scripts/index.js
+++ fiverr-vcmohan/scripts/index.js
@@ -1,34 +1,39 @@
-var sectionScenes = [];
-var timer;
+var sectionScenes = [], timer, windowHeigth, windowWidth;
 
 function loaded () {
-  var windowHeigth = window.innerHeight;
+  windowHeigth = window.innerHeight;
+  windowWidth = window.innerWidth;
   // cache scenes
   sectionScenes.push(document.getElementById('scene1'));
   sectionScenes.push(document.getElementById('scene2'));
-  sectionScenes[0].style.height = windowHeigth + 'px';
-  sectionScenes[1].style.height = windowHeigth + 'px';
+  if (windowWidth > 768) {
+    sectionScenes[0].style.height = windowHeigth + 'px';
+    sectionScenes[1].style.height = windowHeigth + 'px';
+  } else {
+    sectionScenes[0].style.height = windowHeigth * 0.7 + 'px';
+    sectionScenes[1].style.height = windowHeigth * 0.7 + 'px';
+  }
   // init iscroll
   myScroll = new IScroll('#wrapper', {
     probeType: 3,
     mouseWheel: true,
-    snap: true,
+    // snap: true,
     scrollbars: true,
 		interactiveScrollbars: true,
 		shrinkScrollbars: 'scale',
 		fadeScrollbars: true
   });
   myScroll.on('scroll', handleScroll);
-  sectionScenes[0].className = 'scene scene-in';
 }
 
 function handleScroll () {
-  const index = this.currentPage.pageY;
+  var scrollTop = -this.y + windowHeigth * 0.7;
   clearTimeout(timer);
   timer = setTimeout(function(){
-    sectionScenes[index].className = 'scene scene-in';
     for (var i = 0; i < sectionScenes.length; i++) {
-      if (i !== index) {
+      if (scrollTop > sectionScenes[i].offsetTop) {
+        sectionScenes[i].className = 'scene scene-in';
+      } else {
         sectionScenes[i].className = 'scene';
       }
     }
