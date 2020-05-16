c031bc33cb05eb8e9e157adaabe3f4fd35f21d7c
howardwzh <howardwzh@163.com>
2020-05-16 12:20:09 +0800

    chore:visitor增加用户配置项

--- dist-visitor/config.js
+++ dist-visitor/config.js
@@ -1,3 +1,19 @@
 window.visitorConfig = {
-  baseURL: 'http://47.115.140.192:8763/api/campus/visitor/v2.0/m/'
+  baseURL: 'http://47.115.140.192:8763/api/campus/visitor/v2.0/m/',
+  visitReasonGroup: [{
+    label: '拜访',
+    value: 1
+  }, {
+    label: '面试',
+    value: 2
+  }, {
+    label: '其他',
+    value: 3
+  }],
+  statusDictionary: {
+    1: '待审核',
+    2: '待访问',
+    3: '已完成',
+    4: '已失效'
+  }
 }
